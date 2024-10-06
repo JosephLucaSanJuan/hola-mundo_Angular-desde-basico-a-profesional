30ea1baa848fce9c265252b65140a18f76c55cb9
JosephLucaSanJuan <jurassicjoe03@gmail.com>
2024-10-06 12:28:20 +0200

    primer_commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,12 +2,29 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
-  selector: 'app-root',
-  standalone: true,
-  imports: [RouterOutlet],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+    selector: 'app-root',
+    //standalone: true,
+    //imports: [RouterOutlet],
+    template: `
+      <button (click)="save($event)">Guardar</button>
+    `,
+    styles: [`/*./app.component.css
+      .activo{
+        background: green
+      }*/
+    `]
 })
 export class AppComponent {
-  title = 'hola-mundo';
+    //mensaje = 'Curso de ANgular';
+    /*nombre = 'Camila'
+
+    getNombre(){
+      return this.nombre
+    }
+
+    isActive = true*/
+
+    save(e: any){
+        console.log(e)
+    }
 }
---
+++ src/app/cursos/cursos.component.ts
@@ -0,0 +1,17 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-cursos',
+  //standalone: true,
+  //imports: [],
+  template: '<h1>Mis Cursos</h1>',
+  styleUrls: ['./cursos.component.css']
+})
+export class CursosComponent implements OnInit {
+
+  constructor() {}
+
+  ngOnInit() {
+      
+  }
+}
